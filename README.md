0edbd6620b112adcd434f9ec71c1ac92676e142f
LAN Xingcan <lanxingcan@gmail.com>
2016-08-11 12:25:52 +0800

    Format freeze, v0.1.0

---
+++ README.md
@@ -0,0 +1,92 @@
+# Timed Unique Identifier
+
+## Introduction
+This is a variation of UUID version 1 defined in RFC4412. 
+
+The design goals of this variation is to make identifier growth as time
+elapses, which is index-friendly for database.
+## Install
+```bash
+npm install tuid
+```
+## Usage
+
+```javascript
+var tuid = requier('tuid');
+var generator = tuid.Generator(); // create a generator with random node id
+var id = generator.generateSync();
+console.log(id.toString()); // prints: 00001347-c645-f034-000e-1211417da52c
+console.log(id.toBuffer().toString('hex')); // prints: 00001347c645f034000e1211417da52c
+```
+### API
+#### Generator
+* `constructor([node: Buffer])`
+  Constructor, take an optional buffer parameter, the buffer should be 
+  at least 6 byte. Typically it should be the MAC address of one network 
+  interface of the host machine. If the node buffer is not presented,
+  random 6 bytes will be fetched via `crypto.getPseudoRandomBytes`.
+* `generateSync(): Identifier` 
+  Generate a Identifier synchronously. Note if identifier is generated 
+  too frequently, exceeding 65536000ops/1ms, an exception will be throw
+  because due to conflict.
+*  `generate(): Promise<Identifier>`
+  Generate a Identifier. The returned promise is resolved immediately, 
+  unless the identifier generation is exceeding 65536000ops/1ms, in such
+  situation, the promise will defer to a suitable time. 
+## Implementation Detail
+The identifier layout: 
+
+  <table>
+    <thead>
+      <td>Timestamp High 32bit</td>
+      <td>Timestamp Middle 16bit</td>
+      <td>Version (0xf) and Timestamp Low 12bit</td>
+      <td>Counter</td>
+      <td>Node 48bit</td>
+    </thead>
+    <tbody>
+      <tr>
+        <td>0000134</td>
+        <td>b916</td>
+        <td><strong>f</strong>054</td>
+        <td>0000</td>
+        <td>bdd077ed01e5</td>
+      </tr>
+    </tbody>
+  </table>
+  
+As show in above:
+
+1. the version changed to `f` to indicates that this is 
+not a standard UUID implementation as currently only v1, v3, 4 and v5 is
+defined. 
+
+2. Timestamp are encoded into a identifier from higher bit 
+to lower bit in network byte order, to ensure the identifier growth when
+treated as a byte string as time elapses. The definition of timestamp is
+same with the ones defined in UUID v1, which is count of 100-nanoseconds 
+since Gregorian Epoch (Oct. 15th, 1587). For system lacks of high 
+resolution timer, such as Javascript, the lower bit of timestamp can be 
+emulated by counter. 
+
+3. Counter field replaces the clock sequence high field and clock
+sequence low field to keep it simple.
+
+4. Node field remains same with UUID v1.
+
+## License
+```
+Copyright (C) 2016 LAN Xingcan
+All right reserved
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+```
+
--- index.ts
+++ index.ts
@@ -1,23 +1,58 @@
 /// <reference path="./typings/index.d.ts" />
 import * as crypto from 'crypto';
+import * as Promise from 'bluebird';
+
+const regexp = /(.{8})(.{4})(.{4})(.{4})(.{12})/;
+const VERSION = 0xF0000000;
+const MILLISECONDS_SINCE_GREGORIAN_EPOCH = 12219292800000;
+const MAX_ID_PER_MILLISECONDS = 655360000 ;
 
 export class Generator {
     private counter = 0;
     private node: Buffer;
+    private lastTimestamp: number;
 
     constructor(node?: Buffer) {
         if (node && node.length >= 6)
             this.node = node.slice(0,6);
         else
             this.node = crypto.pseudoRandomBytes(6);
+        this.lastTimestamp = 0;
     }
 
-    generate(): Identifier{
-        return new Identifier(Date.now(), this.counter++, this.node);
+    generateSync(): Identifier{
+        let counter = this.counter++;
+        let milliseconds = Date.now() + MILLISECONDS_SINCE_GREGORIAN_EPOCH;
+        if (counter == MAX_ID_PER_MILLISECONDS) {
+            if (milliseconds == this.lastTimestamp)
+                throw new Error('Too many identifier generated');
+            else
+                this.counter = 0;
+        } else {
+            this.counter &= 0xFFFF;
+        }
+        this.lastTimestamp = milliseconds;
+        return new Identifier(milliseconds, counter >>> 16, counter & 0xFFFF, this.node);
+    }
+
+
+    generate(): Promise<Identifier> {
+        let generation = done => {
+
+            let counter = this.counter ++;
+            let milliseconds = Date.now() + MILLISECONDS_SINCE_GREGORIAN_EPOCH;
+            if (counter == MAX_ID_PER_MILLISECONDS) {
+                if (milliseconds == this.lastTimestamp && counter == MAX_ID_PER_MILLISECONDS)
+                    return setImmediate(generation);
+                else
+                    this.counter = 0;
+            }
+            this.lastTimestamp = milliseconds;
+            done(new Identifier(milliseconds, 0, this.counter++, this.node));
+        };
+        return new Promise<Identifier>(generation);
     }
 }
-const regexp = /(.{8})(.{4})(.{4})(.{4})(.{12})/;
-const VERSION = 0xF0000000;
 //
 // Timestamp(ms): 48bit
 // Version  : 4bit
@@ -26,11 +61,24 @@ const VERSION = 0xF0000000;
 export class Identifier {
     private value: Buffer;
 
-    constructor (timestamp: number, counter: number, node: Buffer) {
+    /**
+     *
+     * @param milliseconds
+     * @param nanoseconds
+     * @param counter
+     * @param node
+     */
+    constructor (milliseconds: number, nanoseconds: number, counter: number, node: Buffer) {
+        let timestamp = milliseconds & 0xFFFFFFFF;
+        timestamp *= 10000;
+        timestamp += nanoseconds;
         this.value = new Buffer(16);
-        this.value.writeUInt32BE((timestamp >> 12 >> 16) & 0xFFFFFFFF, 0);
-        this.value.writeUInt16BE((timestamp >> 12) & 0xFFFF, 4);
-        this.value.writeUInt32BE((VERSION + ((timestamp & 0xFF) << 16) + (counter & 0xFFFF)), 6);
+        let timestampHigh = timestamp / 0x100000000;
+        timestamp = (timestamp & 0xFFFFFFFF) >>> 0;
+        this.value.writeUInt32BE(timestampHigh, 0); // High 32bit
+        this.value.writeUInt16BE((timestamp >>> 12) & 0xFFFF, 4); //Middle 16bit
+        let timestampLow = timestamp & 0xFFF;   //Low 12bit
+        this.value.writeUInt32BE((VERSION + (timestampLow << 12) + (counter & 0xFFFF)), 6);
         if (node == null || node.length < 6)
             node = crypto.pseudoRandomBytes(6);
         node.copy(this.value, 10, 0, 6);
